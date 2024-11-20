76e5bee51e140ddbf94c0767f56cefc1454cb380
agoesset <agussetiawan160802@gmail.com>
2024-11-21 02:04:24 +0700

    edit migration

--- database/migrations/2024_11_01_065252_create_watch_rings_table.php
+++ database/migrations/2024_11_01_065252_create_watch_rings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image');
-            $table->text('desc');
+            $table->text('desc')->nullable();
             $table->timestamps();
         });
     }
